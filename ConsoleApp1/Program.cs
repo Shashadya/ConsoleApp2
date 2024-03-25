3a72078818328c1ef9998f32a1149b4bb2b53c2a
shashadya <shashadya.644@gmail.com>
2024-03-25 10:09:05 +0530

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3 ,4,5,6,7,8,9,10};
+            int[] numbers = { 1, 2, 3 ,4,5,6,7,8,9,10,11};
             int total = 0;
             
             for (int i = 0; i<numbers.Length; i++)
