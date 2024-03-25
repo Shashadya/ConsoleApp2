67ffeba9a2628263c228adf24129e6477b54297b
shashadya <shashadya.644@gmail.com>
2024-03-25 11:13:47 +0530

    new

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 ,10};
+            int[] numbers = { 10, 20, 30 ,40 ,50 ,60 ,70 ,80 ,90 ,100};
             int total = 0;
             
             for (int i = 0; i<numbers.Length; i++)
